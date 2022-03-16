6be19f4a2d448a414dc4d065e762f0f171141387
gumjung <89791574+BlackDoGumjung@users.noreply.github.com>
2022-03-16 19:22:04 +0900

    Update 2441.js

--- 백준/2441.js
+++ 백준/2441.js
@@ -1,2 +1,23 @@
 // https://www.acmicpc.net/problem/2441, 별 찍기 - 4
 // node 2441
+
+
+for(let i = 0; i < 5; i++ ){
+    console.log(' '.repeat(i) + '*'.repeat(5-i));
+}
+
+['*', '*', '*', '*', '*'].forEach(function(e,i){
+    console.log(' '.repeat(i) + '*'.repeat(5-i));
+})
+
+for (var i = 0; i < 5; i++) {
+    var string = "";
+  
+    for (var j = 0; j < i; j++) {
+      string = string+" ";
+    }
+    for (var j = 0; j <= 4-i; j++){
+      string = string+"*";
+  }
+    console.log(string);
+  }
