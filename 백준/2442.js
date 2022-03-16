96af70da3dc6591f51c8ce5c0bb65f433013d983
gumjung <89791574+BlackDoGumjung@users.noreply.github.com>
2022-03-16 19:22:50 +0900

    Update 2442.js

--- 백준/2442.js
+++ 백준/2442.js
@@ -1,2 +1,25 @@
 // https://www.acmicpc.net/problem/2442, 별 찍기 -5
 // node 2442
+
+for(let i = 1; i < 11; i+= 2 ){
+    console.log(' '.repeat((11-i)/2) + '*'.repeat(i));
+}
+
+const arr = ['*','*','*','*','*','*','*','*','*','*','*']
+arr.forEach(function(e,i){
+    if(i % 2 !== 0){
+        console.log(' '.repeat((arr.length - i)/2) + '*'.repeat(i));
+    }  
+})
+
+var star = '';
+for (var i = 0; i < 5; i++) {
+    for (var j = 4; j > i; j--) {
+        star += ' ';
+    }
+    for (var k = 0; k <= (i * 2); k++) {
+        star += '*';
+    }
+    star += '\n';
+}
+console.log(star);
