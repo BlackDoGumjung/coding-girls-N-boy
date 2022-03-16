36b2ace86ebe7ea0b7d2f3a247c1ee1bbdf26377
gumjung <89791574+BlackDoGumjung@users.noreply.github.com>
2022-03-16 19:21:16 +0900

    Update 2440.js

--- 백준/2440.js
+++ 백준/2440.js
@@ -1,2 +1,20 @@
 // https://www.acmicpc.net/problem/2440, 별 찍기 - 3
 // node 2440
+
+for(let i = 5; i > 0; i--){
+    console.log('*'.repeat(i));
+}
+
+const arr = ['*','*','*','*','*']
+arr.forEach(function(e, i){
+    console.log(e.repeat(5-i));
+});
+
+var star = '';
+for (var i = 5; i >= 0; i--) {
+    for (var j = 0; j <= i; j++) {
+        star += '*';
+    }
+    star += '\n';
+}
+console.log(star);
